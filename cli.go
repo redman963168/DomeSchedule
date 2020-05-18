00d97ccd3ec708bfc31c7e57e64a6771ac7ea505
redman963168 <64001030+redman963168@users.noreply.github.com>
2020-05-18 14:32:42 +0900

    version情報追加

--- cli.go
+++ cli.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
 	"os"
 )
+
+var version = "0.0.1"
+
+//引数
+var (
+	verOpt  = flag.Bool("version", false, "Show version info")
+  )
 
 //SlackのAPIトークン、チャンネル名
 var (
@@ -25,11 +33,19 @@ type CLI struct {
 
 //Run 実処理
 func (c *CLI) Run(args []string) int {
+	//version
+	flag.Parse()
+	if *verOpt  {
+		fmt.Fprintln(c.outStream, version)
+		return ExitCodeOK
+	}
+	//スケジュール取得
 	sche, err := GetSchedule()
 	if err != nil {
 		fmt.Fprintln(c.errStream, os.ErrInvalid, err)
 		return ExitCodeError
 	}
+	//スケジュールPOST
 	if err = PostMsg(sche); err != nil {
 		fmt.Fprintln(c.errStream, os.ErrInvalid, err)
 		return ExitCodeError
